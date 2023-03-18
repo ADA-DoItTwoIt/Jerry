05c482e3263c20e33ac3afa2cf416782c2d99100
Jhwan <ehgp77@naver.com>
2023-03-18 13:19:14 +0900

    Initial Commit

---
+++ dailyApp/dailyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyAppApp.swift
+//  dailyApp
+//
+//  Created by 주환 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct dailyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
